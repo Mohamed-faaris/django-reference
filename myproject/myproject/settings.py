1af64f270b5314de7ba2454b86b41da0f80f0533
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-05-16 00:36:18 +0530

    add styling

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7uh7&e#gl_)g4i&3z!4lec$vc%4zu*xamq9i2+o_54s@of&9pi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','127.0.0.1']
 
 
 
